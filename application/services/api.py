811ef0fe1236b9a4fa91327bed78ca084eb99604
Julien Bernard <julien.bernard.iphone@gmail.com>
2017-02-28 23:36:24 +0000

    add opta command

--- application/services/api.py
+++ application/services/api.py
@@ -1,3 +1,5 @@
+import json
+
 from nameko.web.handlers import http
 from nameko.rpc import RpcProxy
 
@@ -7,10 +9,11 @@ class ApiService(object):
     
     twitter_rpc = RpcProxy('tweet_collector')
     rss_rpc = RpcProxy('rss_collector')
+    opta_rpc = RpcProxy('opta_collector')
     
     @http('POST','/api/v1/command/twitter/add_user')
     def twitter_add_user(self, request):
-        user_id = request.get_data(as_text=True)
+        user_id = json.loads(request.get_data(as_text=True))['user_id']
         
         inserted_id = self.twitter_rpc.add_user(user_id)
         
@@ -18,11 +21,21 @@ class ApiService(object):
         
     @http('POST','/api/v1/command/rss/add_feed')
     def rss_add_feed(self, request):
-        feed_url = request.get_data(as_text=True)
+        feed_url = json.loads(request.get_data(as_text=True))['feed_url']
         
         self.rss_rpc.add_feed(feed_url)
         
         return 'RSS feed {} inserted'.format(feed_url)
+        
+    @http('POST', '/api/v1/command/opta/add_f1')
+    def opta_add_f1(self, request):
+        data = json.loads(request.get_data(as_text=True))
+        season_id = data['season_id']
+        competition_id = data['competition_id']
+        
+        self.opta_rpc.add_f1(season_id, competition_id)
+        
+        return 'Opta F1 for season {} and competition {} inserted'.format(season_id, competition_id)
         
         
     
