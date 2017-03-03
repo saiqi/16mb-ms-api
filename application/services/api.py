9cee719e9de635c5d3ce89c2a897506223e30acf
julien <julien.bernard.iphone@gmail.com>
2017-03-03 13:29:34 +0100

    update all f9 command

--- application/services/api.py
+++ application/services/api.py
@@ -11,7 +11,7 @@ class ApiService(object):
     rss_rpc = RpcProxy('rss_collector')
     opta_rpc = RpcProxy('opta_collector')
     
-    @http('POST','/api/v1/command/twitter/add_user')
+    @http('POST', '/api/v1/command/twitter/add_user')
     def twitter_add_user(self, request):
         user_id = json.loads(request.get_data(as_text=True))['user_id']
         
@@ -19,7 +19,7 @@ class ApiService(object):
         
         return 'Twitter user {} inserted'.format(inserted_id)
         
-    @http('POST','/api/v1/command/rss/add_feed')
+    @http('POST', '/api/v1/command/rss/add_feed')
     def rss_add_feed(self, request):
         feed_url = json.loads(request.get_data(as_text=True))['feed_url']
         
@@ -36,6 +36,13 @@ class ApiService(object):
         self.opta_rpc.add_f1(season_id, competition_id)
         
         return 'Opta F1 for season {} and competition {} inserted'.format(season_id, competition_id)
-        
-        
-    
+
+    @http('POST', '/api/v1/command/opta/update_all_f9')
+    def opta_add_f1(self, request):
+        data = json.loads(request.get_data(as_text=True))
+        season_id = data['season_id']
+        competition_id = data['competition_id']
+
+        self.opta_rpc.update_all_f9(season_id, competition_id)
+
+        return 'Opta F9 for season {} and competition {} inserted'.format(season_id, competition_id)
