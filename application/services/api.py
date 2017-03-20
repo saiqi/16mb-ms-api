0cd6ac82099f536d80355ec9570b735e62b419ed
julien <julien.bernard.iphone@gmail.com>
2017-03-21 00:13:03 +0100

    asynchronous call

--- application/services/api.py
+++ application/services/api.py
@@ -1,75 +1,89 @@
 import json
 
 from nameko.web.handlers import HttpRequestHandler
-from nameko.rpc import RpcProxy
+from nameko.standalone.rpc import ClusterRpcProxy
+from nameko.dependency_providers import Config
 from werkzeug.exceptions import BadRequest, Unauthorized, Forbidden
 import jwt
 
 
 class HttpAuthenticatedRequestHandler(HttpRequestHandler):
-    
     def handle_request(self, request):
-        
+
         if not request.headers.get('Authorization'):
             raise Unauthorized()
-            
+
         token = request.headers.get('Authorization')
-        
+
         try:
             payload = jwt.decode(token, self.container.config['SECRET_KEY'], algorithms='HS256')
         except jwt.DecodeError:
             raise Unauthorized()
         except jwt.ExpiredSignatureError:
             raise Unauthorized()
-            
+
         if payload['role'] not in ('admin', 'write',):
             raise Forbidden()
-            
-        return super(HttpAuthenticatedRequestHandler, self).handle_request(request)  
+
+        return super(HttpAuthenticatedRequestHandler, self).handle_request(request)
 
 
-http=HttpAuthenticatedRequestHandler.decorator
+http = HttpAuthenticatedRequestHandler.decorator
 
 
 class ApiService(object):
     name = 'api_service'
-    
-    twitter_rpc = RpcProxy('tweet_collector')
-    rss_rpc = RpcProxy('rss_collector')
-    opta_rpc = RpcProxy('opta_collector')
-    
+
+    config = Config()
+
     @http('POST', '/api/v1/command/twitter/add_user')
     def twitter_add_user(self, request):
-        user_id = json.loads(request.get_data(as_text=True))['user_id']
-        
-        inserted_id = self.twitter_rpc.add_user(user_id)
-        
-        return 'Twitter user {} inserted'.format(inserted_id)
-        
+        try:
+            user_id = json.loads(request.get_data(as_text=True))['user_id']
+        except:
+            raise BadRequest()
+
+        with ClusterRpcProxy(self.config) as rpc:
+            rpc.tweet_collector.add_user.call_async(user_id)
+
+            return 'Inserting twitter user {} ...'.format(user_id)
+
     @http('POST', '/api/v1/command/rss/add_feed')
     def rss_add_feed(self, request):
-        feed_url = json.loads(request.get_data(as_text=True))['feed_url']
-        
-        self.rss_rpc.add_feed(feed_url)
-        
-        return 'RSS feed {} inserted'.format(feed_url)
-        
+        try:
+            feed_url = json.loads(request.get_data(as_text=True))['feed_url']
+        except:
+            raise BadRequest()
+
+        with ClusterRpcProxy(self.config) as rpc:
+            rpc.rss_collector.add_feed.call_async(feed_url)
+
+            return 'Inserting RSS feed {} ...'.format(feed_url)
+
     @http('POST', '/api/v1/command/opta/add_f1')
     def opta_add_f1(self, request):
-        data = json.loads(request.get_data(as_text=True))
-        season_id = data['season_id']
-        competition_id = data['competition_id']
-        
-        self.opta_rpc.add_f1(season_id, competition_id)
-        
-        return 'Opta F1 for season {} and competition {} inserted'.format(season_id, competition_id)
+        try:
+            data = json.loads(request.get_data(as_text=True))
+            season_id = data['season_id']
+            competition_id = data['competition_id']
+        except:
+            raise BadRequest()
+
+        with ClusterRpcProxy(self.config) as rpc:
+            rpc.opta_collector.add_f1.call_async(season_id, competition_id)
+
+            return 'Inserting Opta F1 for season {} and competition {} ...'.format(season_id, competition_id)
 
     @http('POST', '/api/v1/command/opta/update_all_f9')
     def opta_update_all_f9(self, request):
-        data = json.loads(request.get_data(as_text=True))
-        season_id = data['season_id']
-        competition_id = data['competition_id']
+        try:
+            data = json.loads(request.get_data(as_text=True))
+            season_id = data['season_id']
+            competition_id = data['competition_id']
+        except:
+            raise BadRequest()
 
-        self.opta_rpc.update_all_f9(season_id, competition_id)
+        with ClusterRpcProxy(self.config) as rpc:
+            rpc.opta_collector.update_all_f9.call_async(season_id, competition_id)
 
-        return 'Opta F9 for season {} and competition {} inserted'.format(season_id, competition_id)
+            return 'Inserting Opta F9 for season {} and competition {} ...'.format(season_id, competition_id)
