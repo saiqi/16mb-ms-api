fc28f69763a6b1646d336cbc36d93ceb3b28d761
julien <julien.bernard.iphone@gmail.com>
2017-05-18 22:33:21 +0200

    complex method from crontask

--- application/services/api.py
+++ application/services/api.py
@@ -36,30 +36,6 @@ class ApiService(object):
 
     config = Config()
 
-    @http('POST', '/api/v1/command/twitter/add_user')
-    def twitter_add_user(self, request):
-        try:
-            user_id = json.loads(request.get_data(as_text=True))['user_id']
-        except:
-            raise BadRequest()
-
-        with ClusterRpcProxy(self.config) as rpc:
-            rpc.tweet_collector.add_user.call_async(user_id)
-
-            return 'Inserting twitter user {} ...'.format(user_id)
-
-    @http('POST', '/api/v1/command/rss/add_feed')
-    def rss_add_feed(self, request):
-        try:
-            feed_url = json.loads(request.get_data(as_text=True))['feed_url']
-        except:
-            raise BadRequest()
-
-        with ClusterRpcProxy(self.config) as rpc:
-            rpc.rss_collector.add_feed.call_async(feed_url)
-
-            return 'Inserting RSS feed {} ...'.format(feed_url)
-
     @http('POST', '/api/v1/command/opta/add_f1')
     def opta_add_f1(self, request):
         try:
@@ -84,7 +60,7 @@ class ApiService(object):
             raise BadRequest()
 
         with ClusterRpcProxy(self.config) as rpc:
-            rpc.opta_collector.update_all_f9.call_async(season_id, competition_id)
+            rpc.crontask.load_opta_soccer.call_async(season_id, competition_id)
 
             return 'Inserting Opta F9 for season {} and competition {} ...'.format(season_id, competition_id)
 
@@ -132,17 +108,8 @@ class ApiService(object):
         except:
             raise BadRequest()
 
-        to_parse = '|'.join([raw_formula, name, is_success_rate, is_negative])
-
         with ClusterRpcProxy(self.config) as rpc:
-            formulas = rpc.formula_parser.parse([to_parse])
-            rpc.formulastore.add_formula(raw_formula, name, is_success_rate, is_negative, context, category)
-
-            if context == 'soccer':
-                rpc.datastore.delete('SOCCER_ADVANCED_PLAYERSTAT', {'FORMULA_ID': name})
-                query = rpc.dsas_query.get_playerstats_query(formulas)
-                rpc.datastore.insert_from_select.call_async('SOCCER_ADVANCED_PLAYERSTAT', query['query'],
-                                                            query['parameters'])
+            rpc.crontask.add_formula.call_async(raw_formula, name, is_success_rate, is_negative, context, category)
 
             return 'Inserting new formula {} ...'.format(name)
 
@@ -155,11 +122,6 @@ class ApiService(object):
             raise BadRequest()
 
         with ClusterRpcProxy(self.config) as rpc:
-            formula = rpc.formulastore.get_formula(formula_id)
-
-            if formula['context'] == 'soccer':
-                rpc.datastore.delete.call_async('SOCCER_ADVANCED_PLAYERSTAT', {'FORMULA_ID': formula['name']})
-
-            rpc.formulastore.delete_formula(formula_id)
+            rpc.crontask.delete_formula.call_async(formula_id)
 
             return 'Deleting formula {}'.format(formula_id)
